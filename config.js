c2025f42ee4894a022c922f6201869752b312c4d
shuffletube <xproj2002@gmail.com>
2026-09-25 19:11:08 +0600

    test

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
    Change ONLY these 3 values before publishing a new coin.
    ============================================================ */
 window.SHUFFLETUBE_CONFIG = {
-  CONTRACT_ADDRESS: '',
-  X_URL: 'https://x.com/i/communities/1915257429442318842',
-  PUMP_URL: 'https://pump.fun/coin/6Zv23y8XGohEhA2KKjrCduhQTtV15HF7R4BAc6v7pump'
+  CONTRACT_ADDRESS: 'soon',
+  X_URL: 'https://x.com/Shuffletube_',
+  PUMP_URL: 'https://pump.fun/'
 };
